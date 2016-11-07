f67af681561a270392df5e29e7d2f695edb62298
hayottedotcom <adrien@hayotte.com>
2016-11-07 09:42:42 +0100

    FirstPush

---
+++ app/src/main/java/fr/esiea/inf4042/projet/inf4042_hayotte_cosson/MainActivity.java
@@ -0,0 +1,13 @@
+package fr.esiea.inf4042.projet.inf4042_hayotte_cosson;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
